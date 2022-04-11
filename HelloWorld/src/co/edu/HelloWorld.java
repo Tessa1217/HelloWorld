26bff4592acfb6cda2dcd8cef3ca6cd0cbc64d97
admin <admin@YD2-04>
2022-04-11 13:45:09 +0900

    first commit - hello world

---
+++ HelloWorld/src/co/edu/HelloWorld.java
@@ -0,0 +1,9 @@
+package co.edu;
+
+public class HelloWorld { //클래스
+		public static void main(String[] args) { //메소드 (클래스 실행 시 가장 먼저 실행되는 메소드 = 메인 메소드)
+			System.out.println("Hello, World");
+			System.out.println("안녕하세요, 권유진입니다.");
+			System.out.println("안녕히가세요.");
+		}
+}
