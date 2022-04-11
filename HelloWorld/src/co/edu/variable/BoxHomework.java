b2fdabed91364b199087e5820a8eb7ffbcc07f41
tessa <tessa@localhost>
2022-04-11 21:39:33 +0900

    third commit - orange box

--- HelloWorld/src/co/edu/variable/BoxHomework.java
+++ HelloWorld/src/co/edu/variable/BoxHomework.java
@@ -3,6 +3,7 @@ package co.edu.variable;
 public class BoxHomework {
 	// 오렌지 127개, 10개씩 담을 수 있는 상자와 5개씩 담을 수 있는 상자 구하기
 	public static void main(String[] args) {
+		
 		int orange = 127;
 		int tenbox = orange/10;
 		int fivebox;
@@ -12,6 +13,16 @@ public class BoxHomework {
 			fivebox = (orange%10)/5 + 1;
 		}
 		System.out.printf("10개씩 들어가는 박스: %d\n", tenbox);
-		System.out.printf("5개씩 들어가는 박스: %d", fivebox);
+		System.out.printf("5개씩 들어가는 박스: %d\n", fivebox);
+		
+		int orange2 = 127;
+		int tenbox2 = orange2/10;
+		int orangeRemain = orange%10;
+		int fivebox2 = orangeRemain/5;
+		if(orangeRemain%5 > 0) {
+			fivebox2 += 1;
+		}
+		System.out.printf("10개씩 들어가는 박스: %d\n", tenbox2);
+		System.out.printf("5개씩 들어가는 박스: %d\n", fivebox2);
 	}	
 }
