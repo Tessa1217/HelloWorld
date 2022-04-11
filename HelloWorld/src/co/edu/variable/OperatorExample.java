7555f8edc8fc3e8eb6862b245b81dd3f292a5dbb
tessa <tessa@localhost>
2022-04-11 19:44:51 +0900

    second commit - operator(%) and exchanging money example

--- HelloWorld/src/co/edu/variable/OperatorExample.java
+++ HelloWorld/src/co/edu/variable/OperatorExample.java
@@ -8,6 +8,7 @@ public class OperatorExample {
 		System.out.println(a/b);
 		System.out.println(a%b);
 		
+		//금액을 지폐 + 동전으로 교환하였을 때 각각의 지폐/동전의 수
 		int money_origin = 116400;
 		int money_left = 0;
 		int bill50 = money_origin/50000;
@@ -24,8 +25,26 @@ public class OperatorExample {
 		money_left %= 100;
 		
 		System.out.printf("금액 %d원은 5만원권 %d장, 1만원권 %d장, 5천원권 %d장, "
-				+ "1천원권 %d장, 5백원 %d개, 1백원 %d개로 교환할 수 있습니다.", money_origin, 
+				+ "1천원권 %d장, 5백원 %d개, 1백원 %d개로 교환할 수 있습니다.\n", money_origin, 
 				bill50, bill10, bill5, bill1, coin5, coin1);
+		
+		// 금액 교환 2 
+		int moneyOrigin = 75400;
+		int coin100 = moneyOrigin/100;
+		int coin500 = coin100/5;
+		coin100 %= 5;
+		int bill1000 = coin500/2;
+		coin500 %= 2;
+		int bill5000 = bill1000/5;
+		bill1000 %= 5;
+		int bill10000 = bill5000/2;
+		bill5000 %= 2;
+		int bill50000 = bill10000/5;
+		bill10000 %= 5;
+		System.out.printf("금액 %d원은 5만원권 %d장, 1만원권 %d장, 5천원권 %d장, "
+				+ "1천원권 %d장, 5백원 %d개, 1백원 %d개로 교환할 수 있습니다.\n", moneyOrigin, 
+				bill50000, bill10000, bill5000, bill1000, coin500, coin100);
+		
 		
 	}
 }
