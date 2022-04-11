b7264389ddc9d67cc89f50743b116057f1342fa6
admin <admin@YD2-04>
2022-04-11 14:25:13 +0900

    first commit - if

---
+++ HelloWorld/src/co/edu/variable/IfExample2.java
@@ -0,0 +1,21 @@
+package co.edu.variable;
+
+public class IfExample2 {
+	public static void main(String[] args) {
+		int num1, num2;
+		num1 = 150;
+		num2 = 248;
+		
+		num1 = 350;
+		num2 = 127;
+		
+		num1 = 123;
+		num2 = 123;
+		
+		if(num1 > num2) {
+			System.out.println("큰 수는 " + num1);
+		} else {
+			System.out.println("큰 수는 " + num2);
+		}
+	}
+}
