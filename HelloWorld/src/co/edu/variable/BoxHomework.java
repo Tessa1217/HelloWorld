0518694219bdc29e83881504a9efbfb7a722010c
admin <admin@YD2-04>
2022-04-11 17:49:20 +0900

    first commit - homework1 (box problem)

--- HelloWorld/src/co/edu/variable/BoxHomework.java
+++ HelloWorld/src/co/edu/variable/BoxHomework.java
@@ -4,9 +4,13 @@ public class BoxHomework {
 	// 오렌지 127개, 10개씩 담을 수 있는 상자와 5개씩 담을 수 있는 상자 구하기
 	public static void main(String[] args) {
 		int orange = 127;
-		int tenbox = orange/10;
-		int fivebox = orange/5;
-		System.out.printf("10개씩 담을 수 있는 박스: %d박스\n", tenbox);
-		System.out.printf("5개씩 담을 수 있는 박스: %d박스\n", fivebox);
+		int tenbox = orange / 10;
+		int fivebox = (orange - (10*tenbox))/5;
+		int remain = (orange - (10*tenbox))%5;
+		if(5 > remain & remain > 0) {
+			fivebox += 1;
+		}
+		System.out.printf("10개씩 담을 수 있는 상자: %d", tenbox);
+		System.out.printf("5개씩 담을 수 있는 상자: %d", fivebox);
 	}	
 }
