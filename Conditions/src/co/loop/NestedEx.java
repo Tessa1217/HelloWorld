c43cdc621592380a1b4f14c55001a24d1ba8825c
Tessa <kwonyuj1996@gmail.com>
2022-04-13 00:51:46 +0900

    first commit - nested loop

--- Conditions/src/co/loop/NestedEx.java
+++ Conditions/src/co/loop/NestedEx.java
@@ -15,51 +15,6 @@ public class NestedEx {
 		}
 		
 		System.out.println();
-		
-		// 별찍기
-		int star = 5;
-		for (int i = 1; i <= star; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
-		
-		// 반대로 별찍기
-		for (int x = 1; x <= star; x++) {
-			for (int y = 5; y >= x; y--) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
-		
-		// 별 오른쪽에 찍기
-		for (int a = 1; a <= 5; a++) {
-			for (int c = 4; c >= a; c--) {
-				System.out.print(" ");
-			}
-			for (int b = 1; b <= a; b++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
-		
-		// 별 오른쪽에서 반대로 짝기
-		for (int d = 1; d <= star; d++) {
-			for (int e = 0; e <= star; e++) {
-				System.out.print(" ");
-			}
-			for (int f = 5; f >= d; f-- ) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
 
 	}
 }
