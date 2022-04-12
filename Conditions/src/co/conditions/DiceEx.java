207f4c28baad40dd6cd4b3f7fa0ff036397b6b82
Tessa <kwonyuj1996@gmail.com>
2022-04-13 00:52:03 +0900

    first commit - identifying random value

--- Conditions/src/co/conditions/DiceEx.java
+++ Conditions/src/co/conditions/DiceEx.java
@@ -10,7 +10,7 @@ public class DiceEx {
 			System.out.print("주사위의 눈을 맞춰보세요 >> \n");
 			int guess = scn.nextInt();
 			if(dice == guess) {
-				System.out.println("정답입니다.");
+				System.out.println("정답입니다. 랜덤 주사위 눈은 " + dice + "이었습니다.");
 				break;
 			} 
 			if(dice > guess) {
