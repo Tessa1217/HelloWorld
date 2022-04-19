559732cc4e3d767c316f91c496c67a5e9f5ff4d3
Tessa <kwonyuj1996@gmail.com>
2022-04-19 17:50:55 +0900

    first commit - finding the number of different remains

--- Algorithm/src/baekjoon/array/Remains.java
+++ Algorithm/src/baekjoon/array/Remains.java
@@ -3,6 +3,7 @@ package baekjoon.array;
 public class Remains {
 	public static void main(String[] args) {
 		int[] intAry = { 39, 40, 41, 42, 43, 44, 82, 83, 84, 85 };
+		int cnt = 0;
 		for (int i = 0; i < intAry.length; i++) {
 			intAry[i] %= 42;
 			System.out.println(intAry[i]);
