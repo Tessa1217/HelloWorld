89ee6c4e66c3b49cd3a178c0698d41d338b620d4
Tessa <kwonyuj1996@gmail.com>
2022-04-20 11:30:49 +0900

    second commit - using stream

--- Inheritance/src/edu/collection/FriendServiceStream.java
+++ Inheritance/src/edu/collection/FriendServiceStream.java
@@ -5,57 +5,16 @@ import java.util.stream.Stream;
 
 import edu.inherit.Friend;
 
-public class FriendServiceStream implements FriendService {
+public class FriendServiceStream extends FriendServiceList {
 
-	ArrayList<Friend> friends = new ArrayList<Friend>();
 	Stream<Friend> stream = friends.stream();
 
-	@Override
-	public void addFriend(Friend friend) {
-		friends.add(friend);
-		System.out.println("친구 추가가 완료되었습니다.");
-	}
-
-	@Override
-	public void modFriend(Friend friend) {
-		String sentence = " 수정할 친구를 추가해주세요.";
-		for (int i = 0; i < friends.size(); i++) {
-			if (friends.get(i).getName().equals(friend.getName())) {
-				friends.get(i).setContact(friend.getContact());
-				System.out.println("연락처 수정이 완료되었습니다.");
-			}
-		}
-	}
-
-	@Override
-	public void remFriend(String name) {
-		String sentence = " 삭제할 친구를 추가해주세요.";
-		this.emptyFriend(sentence);
-		for (int i = 0; i < friends.size(); i++) {
-			if (friends.get(i).getName().equals(name)) {
-				friends.remove(i);
-				System.out.println("친구 삭제가 완료되었습니다.");
-			}
-		}
-	}
-
-	@Override
-	public Friend findFriend(String name) {
-		String sentence = " 조회할 친구를 추가해주세요.";
-		this.emptyFriend(sentence);
-		stream.filter(friend -> friend.getName().equals(name))
-				.forEach((friend) -> System.out.println(friend.toString()));
-		/*
-		 * for (int i = 0; i < friends.size(); i++) { if
-		 * (friends.get(i).getName().equals(name)) { return friends.get(i); } } return
-		 * null;
-		 */
-		return null;
-	}
-
+	// Stream으로 전체 리스트 출력
 	@Override
 	public void listFriend() {
-		this.emptyFriend("");
+		super.emptyFriend("");
 		stream.forEach((e) -> System.out.println(e.toString()));
+		System.out.println();
+
 	}
 }
--- Inheritance/src/edu/collection/FriendStream.java
+++ Inheritance/src/edu/collection/FriendStream.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.stream.Stream;
 
 import edu.inherit.Friend;
+import edu.inherit.Gender;
 
 public class FriendStream {
 	public static void main(String[] args) {
 
 		ArrayList<Friend> friends = new ArrayList<Friend>();
-		friends.add(new Friend("박길동", "010-1111"));
-		friends.add(new Friend("김길동", "010-2222"));
+		friends.add(new Friend("박길동", "010-1111", Gender.Male));
+		friends.add(new Friend("김길동", "010-2222", Gender.Male));
 
 		Stream<Friend> stream = friends.stream();
 		stream.forEach((e) -> System.out.println(e.toString()));
