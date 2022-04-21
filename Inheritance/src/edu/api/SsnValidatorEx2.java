941d2c22de86e07f5f97e426de3e688e4623d646
Tessa <kwonyuj1996@gmail.com>
2022-04-22 00:40:03 +0900

    first commit - ssn validation program

---
+++ Inheritance/src/edu/api/SsnValidatorEx2.java
@@ -0,0 +1,13 @@
+package edu.api;
+
+public class SsnValidatorEx2 {
+
+	public static void main(String[] args) {
+		String ssn = "961219-2444444";
+		ssn = ssn.replace("-", "");
+		System.out.println(ssn);
+		String year = ssn.substring(0, 2);
+		System.out.println(year);
+	}
+
+}
