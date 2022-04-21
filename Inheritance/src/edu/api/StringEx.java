e605c2aabaf7074e9af90062353b9f5e2f671f58
Tessa <kwonyuj1996@gmail.com>
2022-04-22 00:39:46 +0900

    second commit - more String methods

--- Inheritance/src/edu/api/StringEx.java
+++ Inheritance/src/edu/api/StringEx.java
@@ -1,5 +1,7 @@
 package edu.api;
 
+import java.io.UnsupportedEncodingException;
+
 public class StringEx {
 	public static void main(String[] args) {
 		// 주민번호 => 생년월일-남/여
@@ -15,8 +17,43 @@ public class StringEx {
 		System.out.println(ssn.substring(2, 4));
 		System.out.println(ssn.substring(7, 8));
 
-		// 주민번호로 생년월일, 성별 판독 프로그램
+		// String 바이트 타입 배열로 반환
+		byte[] bytes = null;
+		try {
+			bytes = ssn.getBytes("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 
+		for (int i = 0; i < bytes.length; i++) {
+			System.out.print(bytes[i] + " ");
+		}
+		System.out.println();
+
+		// 바이트 타입에서 다시 String으로 변환
+		String str = new String(bytes);
+		System.out.println(str);
+
+		// 특정 값의 시작 인덱스 얻기
+		int index = ssn.indexOf("95");
+		System.out.println(index);
+
+		// 문자열 대치하기
+		ssn = ssn.replace("-", "").substring(0, 7);
+		System.out.println(ssn);
+
+		// 대소문자 변환
+		String case1 = "CAse";
+		System.out.println(case1);
+		case1 = case1.toLowerCase();
+		System.out.println(case1);
+		case1 = case1.toUpperCase();
+		System.out.println(case1);
+		case1 = "  CaSE ";
+		System.out.println(case1);
+		// 공백 문자 제거
+		case1 = case1.trim();
+		System.out.println(case1);
 	}
 
 }
