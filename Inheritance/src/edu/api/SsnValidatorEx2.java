4e734970d9e7b25b4c3b79e7d70b0db8b1e6f6c9
Tessa <kwonyuj1996@gmail.com>
2022-04-22 14:06:19 +0900

    first commit - ssn validator 2

--- Inheritance/src/edu/api/SsnValidatorEx2.java
+++ Inheritance/src/edu/api/SsnValidatorEx2.java
@@ -1,5 +1,8 @@
 package edu.api;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class SsnValidatorEx2 {
 
 	public static void main(String[] args) {
@@ -8,6 +11,7 @@ public class SsnValidatorEx2 {
 		System.out.println(ssn);
 		String year = ssn.substring(0, 2);
 		System.out.println(year);
+
 	}
 
 }
